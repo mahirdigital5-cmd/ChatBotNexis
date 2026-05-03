b92b3b21d1a79960ad95f42dab37aa9995814b15
mahirdigital5-cmd <mahirdigital5@gmail.com>
2026-05-03 13:44:08 +0800

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>ChatBotNexis</h1>
+      <p>Bot berhasil jalan.</p>
+    </main>
+  );
+}
