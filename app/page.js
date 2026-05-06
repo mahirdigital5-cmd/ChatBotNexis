4d7263db5a9866ccacdeb0f0396012f66a03ea28
mahirdigital5-cmd <mahirdigital5@gmail.com>
2026-05-06 15:02:03 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>WA Auto Reply Trigger</h1>
+      <p>Bot aktif 🚀</p>
+    </main>
+  );
+}
