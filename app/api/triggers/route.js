8cdce4f2e516119a78f50e4445f63fe2f171843d
mahirdigital5-cmd <mahirdigital5@gmail.com>
2026-05-07 13:42:48 +0800

    Update route.js

--- app/api/triggers/route.js
+++ app/api/triggers/route.js
@@ -2,8 +2,9 @@ export async function GET() {
   return Response.json([
     {
       keyword: "halo",
-      response: "halo jugaa",
-      image: "",
+      response: "ini gambar test",
+      image:
+        "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
       is_active: true,
     },
   ]);
