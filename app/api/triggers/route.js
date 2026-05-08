e858eabf36a10a377087c226f2d7ec0cf758d951
mahirdigital5-cmd <mahirdigital5@gmail.com>
2026-05-08 21:58:58 +0800

    Update route.js

--- app/api/triggers/route.js
+++ app/api/triggers/route.js
@@ -2,9 +2,8 @@ export async function GET() {
   return Response.json([
     {
       keyword: "halo",
-      response: "ini gambar dari cloudinary",
-      image:
-        "https://res.cloudinary.com/dw1oauijs/image/upload/v1778212352/chatbotnexis/a2ky01smsuuptyrltbcu.jpg",
+      response: "bot aktif lagi",
+      image: "",
       is_active: true,
     },
   ]);
